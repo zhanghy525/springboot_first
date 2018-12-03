11e9009304d1375dbee99db32733ef68dd614373
zhanghy <jh_d16136@163.com>
2018-12-03 17:05:25 +0800

    学习测试thyemeleaf遍历语法

--- src/main/java/com/example/first/Controller/IndexController.java
+++ src/main/java/com/example/first/Controller/IndexController.java
@@ -6,7 +6,11 @@ import com.example.first.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -27,5 +31,23 @@ public class IndexController {
         System.out.println(user);
         // 配合thymeleaf，必须使用@Controller，不能使用@RestController
         return "userInfo";
+    }
+
+    @Log("user")
+    @RequestMapping("/user/{id}")
+    String selectUser(Model model, @PathVariable String id,@RequestParam String name,@RequestParam String password) {
+        User user = userService.selectUserById(Integer.parseInt(id));
+        model.addAttribute("user",user);
+        System.out.println(user);
+        System.out.println(name);
+        return "userInfo";
+    }
+
+    @Log("findAll")
+    @RequestMapping("/findAll")
+    String findAll(Model model) {
+        List<User> list = userService.findUserAll(new User());
+        model.addAttribute("list",list);
+        return "findAll";
     }
 }
